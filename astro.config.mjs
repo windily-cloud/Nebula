d7808e8d3ee687b04a414f919c2efee7e1be7d31
cloud <www.4399li@qq.com>
2023-12-17 00:11:46 +0800

    dev: 添加RSS美化、部署到服务器的action

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,61 +7,47 @@ import callouts from "@portaljs/remark-callouts";
 import wikiLinkPlugin from "@portaljs/remark-wiki-link";
 import { getPermalinks } from "@portaljs/remark-wiki-link";
 import remarkTextHighlight from "./src/plugins/remark-text-highlight";
-
 const permalinks = await getPermalinks("./src/content/docs");
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://localhost",
-    integrations: [
-        starlight({
-            title: "Pkmer Publish",
-            // Set English as the default language for this site.
-            // defaultLocale: "en",
-            locales: {
-                // English docs in `src/content/docs/en/`
-                // en: {
-                //     label: "English",
-                // },
-                // Simplified Chinese docs in `src/content/docs/zh-cn/`
-                root: {
-                    label: "简体中文",
-                    lang: "zh-CN",
-                },
-            },
-            social: {
-                github: "https://github.com/withastro/starlight",
-                rss: "https://github.com"
-            },
-            sidebar: [
-                {
-                    label: "规划",
-                    autogenerate: { directory: "规划" },
-                },
-            ],
-            customCss: ["./src/styles/base.css"],
-        }),
-    ],
-    markdown: {
-        remarkPlugins: [
-            remarkGfm,
-            remarkMath,
-            callouts,
-            remarkTextHighlight(),
-            [
-                wikiLinkPlugin,
-                {
-                    pathFormat: "obsidian-short",
-                    permalinks,
-                    hrefTemplate: (permalink) => {
-                        const link = permalink
-                            .replace("src/content/docs", "")
-                            .replace(" ", "-")
-                            .toLowerCase();
-                        return `${link}`;
-                    },
-                },
-            ],
-        ],
-        rehypePlugins: [rehypeMathJaxSvg],
+  site: "https://localhost",
+  integrations: [starlight({
+    title: "Pkmer Publish",
+    // Set English as the default language for this site.
+    // defaultLocale: "en",
+    locales: {
+      // English docs in `src/content/docs/en/`
+      // en: {
+      //     label: "English",
+      // },
+      // Simplified Chinese docs in `src/content/docs/zh-cn/`
+      root: {
+        label: "简体中文",
+        lang: "zh-CN"
+      }
     },
+    social: {
+      github: "https://github.com/withastro/starlight",
+      rss: "http://localhost:4321/rss.xml"
+    },
+    sidebar: [{
+      label: "规划",
+      autogenerate: {
+        directory: "规划"
+      }
+    }],
+    customCss: ["./src/styles/base.css"]
+  })],
+  markdown: {
+    remarkPlugins: [remarkGfm, remarkMath, callouts, remarkTextHighlight(), [wikiLinkPlugin, {
+      pathFormat: "obsidian-short",
+      permalinks,
+      hrefTemplate: permalink => {
+        const link = permalink.replace("src/content/docs", "").replace(" ", "-").toLowerCase();
+        return `${link}`;
+      }
+    }]],
+    rehypePlugins: [rehypeMathJaxSvg]
+  }
 });
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,5 +1,8 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET(context: any) {
     const blog = await getCollection('docs');
@@ -7,12 +10,12 @@ export async function GET(context: any) {
         title: 'Buzz’s Blog',
         description: 'A humble Astronaut’s guide to the stars',
         site: context.site,
+        stylesheet: 'rss/rss.xsl',
         items: blog.map((post) => ({
             title: post.data.title,
             pubDate: new Date(post.data.uid),
             description: post.data.description,
-            // Compute RSS link from post `slug`
-            // This example assumes all posts are rendered as `/blog/[slug]` routes
+            content: sanitizeHtml(parser.render(post.body)),
             link: `/blog/${post.slug}/`,
         })),
     });
